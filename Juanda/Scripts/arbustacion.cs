da2c6efcb6f8971dc0b541643fb8761b88bf9d13
Shaddaii <jdre03052009@gmail.com>
2025-02-20 19:10:09 +0100

    logicas arbusto

---
+++ Juanda/Scripts/arbustacion.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class arbustacion : MonoBehaviour
+{
+    public int comida = 5;
+    public float tiempo;
+    public GameObject[] frutos;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if (comida < 5) 
+        {
+            tiempo += Time.deltaTime;
+            if (tiempo > 10 )
+            {
+                comida++;
+                tiempo = 0;
+            }
+        }
+        for (int i = 0; i < 5; i++) 
+        {
+
+            if (i < 5 - comida)
+            {
+                frutos[i].SetActive(false);
+            }
+            else 
+            {
+                frutos[i].SetActive(true);
+            }
+        }
+
+    }
+
+    public void consumir() 
+    { 
+      if (comida > 0)
+        {
+            comida--;
+        }
+
+        if (comida == 1)
+        {
+           Destroy(gameObject);
+        }
+    }
+
+
+
+}
